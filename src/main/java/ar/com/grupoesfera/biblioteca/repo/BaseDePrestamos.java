a2d997d01345435c97c9b04bcf2fffcd01bfe4aa
Gabriel <Rodriguez>
2019-10-24 14:36:33 -0300

    Se sube el servicio para obtener un prestamo por id

--- src/main/java/ar/com/grupoesfera/biblioteca/repo/BaseDePrestamos.java
+++ src/main/java/ar/com/grupoesfera/biblioteca/repo/BaseDePrestamos.java
@@ -5,11 +5,20 @@ import java.util.List;
 import ar.com.grupoesfera.biblioteca.modelo.Prestamo;
 import ar.com.grupoesfera.main.App;
 
+import javax.transaction.Transactional;
+
 public class BaseDePrestamos {
 
     @SuppressWarnings("unchecked")
     public List<Prestamo> obtenerTodos() {
-        
         return App.instancia().obtenerEntityManager().createQuery("select p from Prestamo p").getResultList();
     }
+
+    public Prestamo obtenerPrestamoPorId(Long id){
+
+        List<Prestamo> prestamos = obtenerTodos();
+        return prestamos.stream().filter( a -> a.getId().equals(id)).findAny().orElse(null);
+
+    }
+
 }
